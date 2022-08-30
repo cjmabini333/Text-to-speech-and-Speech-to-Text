8040eb7694c09c186670c92f083cff017aa826af
unknown <mabinijeoh@gmail.com>
2022-08-30 15:19:20 +0800

    initial commit

---
+++ pyttsx3TTS.py
@@ -0,0 +1,9 @@
+#pip install pyttsx3
+
+import pyttsx3
+
+text_speech = pyttsx3.init()
+
+answer = input("What do you want to convert to speech: ")
+text_speech.say(answer)
+text_speech.runAndWait()
